1b2960b21d0798c08e122dddc7c2cff5005db248
Matheus Bortolotto <matheusbortolol@hotmail.com>
2025-03-14 10:29:49 -0300

    terminando a calculadora

--- untitled/src/main/java/org/example/Calculadora.java
+++ untitled/src/main/java/org/example/Calculadora.java
@@ -1,42 +1,129 @@
 package org.example;
 
-class Calculadora {
-    // Metodo para somar dois inteiros
-    int soma(int a, int b) {
+public class Calculadora {
+    // Somar dois números inteiros
+    int soma(int a, int b){
         return a + b;
     }
 
-    // Metodo para somar três inteiros
-    int soma(int a, int b, int c) {
+    // Somar dois números de ponto flutuante
+    double soma (double a, double b){
+        return a+b;
+    }
+
+    // Somar três números inteiros
+    int soma(int a, int b, int c){
         return a + b + c;
     }
 
-    // Metodo para somar dois números em ponto flutuante
-    double soma(double a, double b) {
-        return a + b;
+    // Somar um array de números inteiros
+    int soma(int [] numeros){
+        int somaTotal = 0;
+        for(int numero: numeros){
+            somaTotal += numero;
+        }
+        return somaTotal;
     }
 
-    public static void main(String[] args) {
-        // Array de notas
-        int[] notas = new int[4];
-
-        // Atribuindo valores
-        notas[0] = 85;
-        notas[1] = 90;
-        notas[2] = 78;
-        notas[3] = 93;
-
-        // Acessando valores
-        System.out.println("Primeira nota: " + notas[0]);
-
-        // Percorrendo um array com for tradicional
-        for (int i = 0; i < notas.length; i++) {
-            System.out.println("Nota " + (i + 1) + ": " + notas[i]);
-        }
-
-        // Usando foreach para percorrer o array
-        for (int nota : notas) {
-            System.out.println("Notas: " + nota);
-        }
+    // Subtrair dois números inteiros
+    int subtrair (int a, int b){
+        return a - b;
     }
+
+    // Subtrair dois números de ponto flutuante
+    double subtrair (double a, double b){
+        return a - b;
+    }
+
+    // Subtrair três números inteiros
+    int subtrair(int a, int b, int c){
+        return a - b - c;
+    }
+
+    // Subtrair um array de números inteiros
+    int subtrair(int a, int b, int c, int d, int e){
+        int [] numeros = new int[] {a,b,c,d,e};
+        int subtracaoTotal = 0;
+        for (int numero: numeros){
+            subtracaoTotal = numero - subtracaoTotal;
+        }
+        return subtracaoTotal;
+    }
+
+    int subtrair(int [] numeros){
+        int subtracaoTotal = numeros[0];
+        for (int i = 1; i < numeros.length; i++){
+            subtracaoTotal -= numeros[i];
+        }
+        return subtracaoTotal;
+    }
+
+    // Multiplicar dois números inteiros
+    int multiplicar(int a, int b){
+        return a*b;
+    }
+
+    // Multiplicar dois números flutuantes
+    double multiplicar(double a, double b){
+        return a*b;
+    }
+
+    // Multiplicar três números inteiros
+    int multiplicar(int a, int b, int c){
+        return (a*b)*c;
+    }
+
+    // Multiplicar números inteiros
+    int multiplicar(int a, int b, int c, int d, int e){
+        int [] numeros = new int[]{a,b,c,d,e};
+        int multTotal = numeros[0];
+        for (int i = 1; i < numeros.length; i++){
+            multTotal -= numeros[i];
+        }
+        return multTotal;
+    }
+
+    int multiplicar(int [] numeros){
+        int resultado = numeros[0];
+        for (int i = 1; i < numeros.length; i++){
+            resultado -= numeros[i];
+        }
+        return resultado;
+    }
+
+
+    // Dividir dois números inteiros
+    int dividir(int a, int b){
+        return a/b;
+    }
+
+    // Dividir dois números flutuantes
+    double dividir(double a, double b){
+        return a/b;
+    }
+
+    // Dividir três números inteiros
+    int dividir(int a, int b, int c){
+        return (a/b)/c;
+    }
+
+    // Dividir números inteiros
+    int dividir(int a, int b, int c, int d, int e){
+        int [] numeros = new int[]{a,b,c,d,e};
+        int divisaoTotal = numeros[0];
+        for (int i = 1; i < numeros.length; i++) {
+            divisaoTotal /= numeros[i];
+        }
+        return divisaoTotal;
+    }
+
+    int dividir(int [] numeros){
+        int divisaoTotal = numeros[0];
+        for (int i = 1; i < numeros.length; i++) {
+            divisaoTotal /= numeros[i];
+        }
+        return divisaoTotal;
+    }
+
+
 }
--- untitled/src/main/java/org/example/Main.java
+++ untitled/src/main/java/org/example/Main.java
@@ -9,5 +9,20 @@ public class Main{
         System.out.println(calc.soma(2,3,4));
         //Chama o método com números em ponto flutuantes
         System.out.println(calc.soma(2.5,3.5));
+        System.out.println(calc.soma(new int[]{1,2,3,5,6,7,8,8,10}));
+        System.out.println(calc.subtrair(2.5,3.5));
+        System.out.println(calc.subtrair(5,3));
+        System.out.println(calc.subtrair(5,3,1));
+        System.out.println(calc.subtrair(new int[]{1,2,3,5,6,7,8,8,10}));
+        System.out.println(calc.multiplicar(1,2));
+        System.out.println(calc.multiplicar(5.5,3.5));
+        System.out.println(calc.multiplicar(1,2,5));
+        System.out.println(calc.multiplicar(1,2,5,8,9));
+        System.out.println(calc.multiplicar(new int[]{1,2,3,5,6,7,8,8,10}));
+        System.out.println(calc.dividir(1,2));
+        System.out.println(calc.dividir(5.5,3.5));
+        System.out.println(calc.dividir(1,2,5));
+        System.out.println(calc.dividir(1,2,5,8,9));
+        System.out.println(calc.dividir(new int[]{1,2,3,5,6,7,8,8,10}));
     }
 }
